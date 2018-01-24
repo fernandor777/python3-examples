1863758627a4ef9fe6318c4b9aa0b16e0e6c5250
Fernando Miño <fernandomino7@gmail.com>
2018-01-24 17:03:34 -0500

    add

---
+++ ex001/test/lifebeingtest.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+import lifebeing
+import datetime
+
+class HumanTest(TestCase):
+    def setUp(self):
+        self.human = Human("Fer", 36)
+        
+    def test_birthage(self):
+        ba = datetime.datetime.now().year - 36
+        human = lifebeing.Human("Fer", 36)
+        self.assertEqual(ba.human.birthyear)
+    
+
