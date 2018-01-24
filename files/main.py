16fec1d440337ffa39f820e53b00e08744e27691
Fernando Miño <fernandomino7@gmail.com>
2018-01-24 12:11:52 -0500

    add

--- files/main.py
+++ files/main.py
@@ -1,6 +1,10 @@
 import fmcsv
 import time
 
+print("This is the name of the script: ", sys.argv[0])
+print("Number of arguments: ", len(sys.argv))
+print("The arguments are: " , str(sys.argv))
+
 fmcsv.save_json("prods-"+str(time.time())+".json")
 
 
